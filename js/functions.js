5fa02995c2ca58b3da79fee13d551b9f7b0d4599
Петрищев <komradlew@ya.ru>
2023-06-10 11:57:10 +0300

    Добавит функции на палиндром и вывод числа из строки

--- js/functions.js
+++ js/functions.js
@@ -2,3 +2,33 @@ function checkStringLength (str, maxLength) {
   return str.length <= maxLength;
 }
 
+function checkStringPalindrome (str) {
+  let normalizedStr = str.replaceAll(' ', '');
+  normalizedStr = normalizedStr.toLowerCase();
+
+  let reverseStr = '';
+
+  for (let i = normalizedStr.length - 1; i >= 0; i--) {
+    reverseStr += normalizedStr[i];
+  }
+
+  return reverseStr === normalizedStr;
+}
+
+function extractDigits (value) {
+// цикл — для перебора полученной строки,
+  const str = value.toString();
+  let newStr = '';
+
+  for (let i = 0; i < str.length; i++) {
+    const digit = parseInt(str[i], 10);
+    if (!Number.isNaN(digit) && digit >= 0 && digit <= 9) {
+      newStr += digit;
+    }
+  }
+
+  if (newStr === '') {
+    return NaN;
+  }
+  return parseInt(newStr, 10);
+}
