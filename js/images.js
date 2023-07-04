1ced2fba2cc9271f00dfa009e1beaa81417d23b0
Петрищев <komradlew@ya.ru>
2023-07-04 11:02:35 +0300

    Подготовит код к новой домашке

--- js/images.js
+++ js/images.js
@@ -1,21 +1,16 @@
-import {getRandomInteger, getRandomId} from './utils.js';
+import {getRandomInteger} from './utils.js';
 import {generateCommentsArray} from './comments.js';
 
-const USEDIMAGEIDS = [];
-
 // Создаёт рандомный объект-фото
-const createDescriptionImage = () => {
-  const id = getRandomId(USEDIMAGEIDS, 25);
-  return {
-    id: id,
-    url: `photos/${id}.jpg`,
-    description: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...',
-    likes: getRandomInteger(15, 200),
-    comments: generateCommentsArray(getRandomInteger(0, 30))
-  };
-};
+const createImage = (id) => ({
+  id: id,
+  url: `photos/${id}.jpg`,
+  description: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...',
+  likes: getRandomInteger(15, 200),
+  comments: generateCommentsArray(getRandomInteger(0, 30))
+});
 
 // Создаёт массив объектов-фото
-const getImages = (numOfImages) => Array.from({length: numOfImages}, createDescriptionImage);
+const generateImagesArray = (countImages) => Array.from({ length: countImages }, (_, i) => createImage(i + 1));
 
-export {getImages};
+export {generateImagesArray};
